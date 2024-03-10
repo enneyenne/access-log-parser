b2851d2102cf135c0e0699aae2b88f822f0beeed
Eugene <enne.0105@yandex.ru>
2024-03-10 21:20:33 +0300

    add file find logic

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,39 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        finder();
+    }
 
-        System.out.println("Введите первое число:");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число:");
-        int secondNumber = new Scanner(System.in).nextInt();
+    public static void finder() {
 
-        int sum = firstNumber + secondNumber;
-        int diff = firstNumber - secondNumber;
-        int multiply = firstNumber * secondNumber;
-        double quotient = (double) firstNumber / secondNumber;
+        int fileCounter = 0;
+        int findCounter = 0;
 
-        System.out.println("Сумма двух чисел: " + sum);
-        System.out.println("Разность двух чисел: " + diff);
-        System.out.println("Произведение двух чисел: " + multiply);
-        System.out.println("Частное двух чисел: " + quotient);
+        for (; ;) {
+
+            System.out.print("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists) {
+                System.out.println("Указан путь к несуществующему файлу! Повторите попытку");
+                System.out.println("Выполнено попыток поиска: " + ++findCounter);
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Указан путь к папке! Повторите попытку");
+                System.out.println("Выполнено попыток поиска: " + ++findCounter);
+                continue;
+            }
+            fileCounter++;
+            findCounter++;
+            System.out.println("Путь указан верно. Это файл " + path + " с номером " + fileCounter);
+            System.out.println("Выполнено попыток поиска: " + findCounter);
+        }
     }
 }
