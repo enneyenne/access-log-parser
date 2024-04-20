5c7fb1751ed06a94830ab67a599ed94aa25b8926
Eugene <enne.0105@yandex.ru>
2024-04-20 14:53:50 +0300

    Обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -34,6 +36,35 @@ public class Main {
             findCounter++;
             System.out.println("Путь указан верно. Это файл " + path + " с номером " + fileCounter);
             System.out.println("Выполнено попыток поиска: " + findCounter);
+
+            try {
+                int minLineLength = Integer.MAX_VALUE;
+                int maxLineLength = 0;
+                int totalLinesCount = 0;
+
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if (length > 1024) {
+                        throw new RuntimeException("Длина строки в файле более 1024 символов");
+                    }
+                    if (length > maxLineLength) {
+                        maxLineLength = length;
+                    }
+                    if (length < minLineLength) {
+                        minLineLength = length;
+                    }
+                    totalLinesCount++;
+                }
+                System.out.println("Общее количество строк в файле: " + totalLinesCount);
+                System.out.println("Длина самой длинной строки в файле: " + maxLineLength);
+                System.out.println("Длина самой короткой строки в файле: " + minLineLength);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
